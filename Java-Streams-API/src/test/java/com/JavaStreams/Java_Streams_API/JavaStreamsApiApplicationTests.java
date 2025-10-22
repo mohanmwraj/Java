569ea2bbe6ab7641a68688ad6995f5ab2ef17a52
Mohan Raj <mohanmwraj@gmail.com>
2025-10-22 14:57:45 +0530

    Initial Commit

---
+++ Java-Streams-API/src/test/java/com/JavaStreams/Java_Streams_API/JavaStreamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaStreams.Java_Streams_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
