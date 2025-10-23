2e4c76bcae69799164aaee10eed7d71c14a9c38a
Mohan Raj <mohanmwraj@gmail.com>
2025-10-23 12:24:42 +0530

    Stream Problems

--- Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem07.java
+++ Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem07.java
@@ -1,7 +1,27 @@
 package com.JavaStreams.Java_Streams_API.problems1to30;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/*
+    Remove duplicate elements from a list
+ */
 public class problem07 {
     public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 2, 3,3, 3, 4);
 
+        //Approach 1
+        List<Integer> distinct = nums.stream()
+                .distinct()
+                .toList();
+        System.out.println(distinct);
+
+        //Approach 2
+        List<Integer> distinct1 = new ArrayList<>(new LinkedHashSet<>(nums));
+        System.out.println(distinct1);
+
+        //Approach 3
+        Set<Integer> distinct2 = nums.stream()
+                .collect(Collectors.toSet());
+        System.out.println(distinct2);
     }
 }
--- Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem10.java
+++ Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem10.java
@@ -1,7 +1,30 @@
 package com.JavaStreams.Java_Streams_API.problems1to30;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+    Find first element in a list
+ */
 public class problem10 {
     public static void main(String[] args) {
 
+        List<String> list = Arrays.asList("a", "b", "c");
+
+        //Approach 1
+        String first = list.stream().findFirst().orElse(null);
+        System.out.println(first);
+
+        //Approach 2
+        String first1 = list.stream()
+                .limit(1)
+                .collect(
+                        Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                l -> l.isEmpty() ? null : l.get(0)
+                        )
+                );
+        System.out.println(first1);
     }
 }
