3c30ee4ad4bdf46b981bf5c67e2853a77fad5ea5
Mohan Raj <mohanmwraj@gmail.com>
2025-10-23 19:12:45 +0530

    Stream Problems

--- Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem21.java
+++ Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem21.java
@@ -1,7 +1,27 @@
 package com.JavaStreams.Java_Streams_API.problems1to30;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/*
+    Limit a stream to the first N numbers
+ */
 public class problem21 {
     public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(10, 20, 30, 40, 50);
+
+        //Approach 1: limit(n) short circuit after n elements
+        List<Integer> first3 = nums.stream()
+                .limit(3)
+                .toList();
+        System.out.println(first3);
+
+        //Approach 2: Index Slicing alternative
+        List<Integer> first3_1 = IntStream.range(0, Math.min(3, nums.size()))
+                .mapToObj(nums::get)
+                .toList();
+        System.out.println(first3_1);
 
     }
 }
--- Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem25.java
+++ Java-Streams-API/src/main/java/com/JavaStreams/Java_Streams_API/problems1to30/problem25.java
@@ -1,7 +1,31 @@
 package com.JavaStreams.Java_Streams_API.problems1to30;
 
+/*
+    Filter a list of strings based on length
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class problem25 {
     public static void main(String[] args) {
+        List<String> words = Arrays.asList(
+                "a", "abcd", "wxyz", "hello"
+        );
+
+        // Approach 1:
+        List<String> longOnes = words.stream()
+                .filter(n -> n.length() > 3)
+                .toList();
+        System.out.println(longOnes);
+
+        // Approach 2: Collectors.filtering()
+        List<String> longOnes1 = words.stream()
+                .collect(Collectors.filtering(s -> s.length() > 3,
+                        Collectors.toList()));
+
+        System.out.println(longOnes1);
 
     }
 }
